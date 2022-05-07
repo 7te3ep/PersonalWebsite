e200bb4aab9fc1230a92defe9f0be98ea915fbaf
7te3ep <7te3ep@gmail.com>
2022-05-07 20:50:35 +0200

    Navigation menu with Js finished (V1)

--- fonctionnality.js
+++ fonctionnality.js
@@ -2,14 +2,30 @@
 
     document.getElementById('Navigate').textContent = "You clicked";
 } */
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
+
+
 let navigateBolean = false;
 function Navigate() {
     if (navigateBolean == false) {
         navigateBolean = true;
-        document.querySelector('#Navigate').textContent = "Close";}
+        document.querySelector('#Navigate').textContent = "Close";
+        document.getElementById('menu').style.transform = "scaleY(1)";
+        document.querySelector('header').style.top = "0rem";}
         else {
             navigateBolean = false;
             document.querySelector('#Navigate').textContent = "Navigate";
+            document.getElementById('menu').style.transform = "scaleY(0)";
+            document.querySelector('header').style.top = "-7rem";
         }   
 }
+
 
